e8aa419e4bf8556ee718b22b094c6c4fc468bbce
JonieFu <joniefu@foxmail.com>
2020-12-17 16:29:51 +0800

    添加了侧边栏

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,18 +1,82 @@
 <template>
-  <div class="container">
-    我是home组件
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container class="home-container">
+    <el-header>
+      <div>
+        <img src="../assets/heima.png" />
+        <span>电竞后台管理系统</span>
+      </div>
+      <el-button type="info">退出</el-button>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">
+        <el-menu
+          background-color="#333744"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>导航一</span>
+            </template>
+            <el-menu-item index="1-1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>导航一</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 <script>
 export default {
+  created() {
+    this.getMenuList()
+  },
   methods: {
     logout() {
       window.sessionStorage.clear()
       this.$router.push('/login')
     },
+    async getMenuList() {
+      const token = window.sessionStorage.getItem('token')
+      console.log(token)
+      const { data: res } = await this.$http.get('menus')
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      this.menulist = res.data
+      console.log(res)
+    },
   },
 }
 </script>
 <style lang="scss" scoped>
+.home-container {
+  height: 100%;
+}
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  font-size: 20px;
+  div {
+    display: flex;
+    align-items: center;
+    span {
+      color: white;
+      margin-left: 10px;
+    }
+  }
+}
+.el-aside {
+  background: #303440;
+}
+.el-main {
+  background: #e8ecef;
+}
 </style>
